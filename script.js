e5a21e486ba00bfbd3c8b502634587adf7e28991
Ladislav Slezák <lslezak@suse.cz>
2022-06-29 14:44:00 +0200

    Added component filter

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
 
 "use strict";
 
+// convert component name to component group to group similar
+// e.g. for "ui-macro" and "qt-ui" use the same "UI" filtering component
+function component_group(name) {
+  if (name == "zconfig" || name == "parser::yum" || name == "Progress++"
+    || name == "parser++" || name == "parser" || name == "FileChecker"
+    || name == "MODALIAS++" || name.match(/^zypp/) || name.match(/^librpm/)) {
+
+    return "libzypp";
+  }
+  else if (name == "zypp::solver" || name.match(/^libsolv/)) {
+    return "Solver";
+  }
+  else if (name == "Pkg" || name == "Pkg++") {
+    return "Pkg-bindings";
+  }
+  else if (name == "bash" || name == "scr" || name.match(/^agent-/) || name.match(/^ag_/)) {
+    return "Agents";
+  }
+  else if (name == "ui" || name == "libycp" || name.match(/^ui-/) || name.match(/-ui$/) || name.match(/^qt-/)) {
+    return "UI";
+  }
+  else if (name == "liby2" || name == "wfm" || name == "liby2") {
+    return "yast2-core"
+  }
+  else if (name == "Interpreter") {
+    return "Ruby"
+  }
+  else {
+    return name;
+  }
+}
+
 function add_line(parent, entry) {
   var node = document.createElement("div");
   node.classList.add("log-level-" + entry.level)
+  node.classList.add("log-component-" + entry.component_group);
   node.classList.add("log-line")
 
   var span_node = document.createElement("span");
@@ -77,6 +110,8 @@ function start_group(node, label) {
 function parse_y2log(name, y2log) {
   document.getElementById("file-header").textContent = "Rendered File \"" + name + "\"";
 
+  var component_groups = new Set();
+
   var parent_node = document.getElementById("content");
   y2log.split("\n").forEach(line => {
     var res = line.match(/^(\d\d\d\d-\d\d-\d\d) (\d\d:\d\d:\d\d) <(\d)> (\w+)\((\d+)\) \[(\S+)\] (.*)/);
@@ -106,6 +141,9 @@ function parse_y2log(name, y2log) {
         parent_node = start_group(parent_node, res[1]);
       }
 
+      entry.component_group = component_group(entry.component);
+      component_groups.add(entry.component_group);
+
       // log group closed
       res = entry.message.match(/^::endgroup::(.*)/);
       if (res) {
@@ -120,6 +158,8 @@ function parse_y2log(name, y2log) {
       append_line(parent_node, line);
     }
   });
+
+  return component_groups;
 }
 
 // load a local file selected by user
@@ -132,12 +172,14 @@ function load_file(e) {
   // clean the previous content
   document.getElementById("content").textContent = "";
   document.getElementById("file-header").textContent = "";
+  document.getElementById("filter-group-components-list").textContent = "";
 
   // HTML5 FileReader
   var reader = new FileReader();
   reader.onload = function (ev) {
     var contents = ev.target.result;
-    parse_y2log(file.name, contents);
+    var groups = parse_y2log(file.name, contents);
+    add_component_filters(groups);
   };
 
   reader.readAsText(file);
@@ -160,11 +202,41 @@ function update_display(event) {
   });
 }
 
+function update_components(checkbox, group) {
+  const new_style = checkbox.checked ? "" : "none";
+  document.querySelectorAll(".log-component-" + group).forEach(node => {
+    node.style.display = new_style;
+  });
+}
+
+function add_component_filters(component_groups) {
+  var parent = document.getElementById("filter-group-components-list")
+  component_groups.forEach(group => {
+
+    var input = document.createElement("input");
+    input.type = "checkbox";
+    input.checked = true;
+    input.onclick = function (e) {
+      update_components(input, group);
+    }
+
+    var span = document.createElement("span");
+    span.classList.add("checkable");
+    span.textContent = group;
+
+    var label = document.createElement("label");
+    label.appendChild(input);
+    label.appendChild(span);
+
+    parent.appendChild(label);
+  });
+}
+
 window.onload = function () {
   document.getElementById("file").addEventListener("change", load_file, false);
 
   // display filtering popup
-  document.getElementById("filter").onclick = function() {
+  document.getElementById("filter").onclick = function () {
     document.getElementById("configuration_popup").checked = true;
   };
 
