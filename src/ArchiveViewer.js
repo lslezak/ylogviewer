6233ee43646cfc3ac3c72910aba15fe188d1bc62
Ladislav Slezák <lslezak@suse.cz>
2023-01-31 17:39:08 +0100

    Unpack XZ archive

--- src/ArchiveViewer.js
+++ src/ArchiveViewer.js
@@ -2,10 +2,44 @@
 import React, { useState, useEffect } from "react";
 import { Bullseye, Spinner } from "@patternfly/react-core";
 
+import { XzReadableStream } from 'xzwasm';
+
 export default function ArchiveViewer({data, name}) {
   const [logData, setLogData] = useState(data);
+  const [originalFileName, setOriginalFileName] = useState(name);
   const [fileName, setFileName] = useState(name);
   const [processing, setProcessing] = useState(false);
+
+  useEffect(() => {
+    console.log("useEffect processing:", processing);
+
+    if (processing) return;
+
+    // xz archive
+    if (fileName.match(/\.xz$/i)) {
+      console.time("Uncompressing " + fileName);
+      setProcessing(true);
+      const stream = new ReadableStream({
+        start: (controller) => {
+          controller.enqueue(new Uint8Array(logData));
+          controller.close();
+        }
+      });
+
+      const decompressedResponse = new Response(
+        new XzReadableStream(stream)
+      );
+
+      decompressedResponse.arrayBuffer().then(done => {
+        console.timeEnd("Uncompressing " + fileName);
+        const newData = new Uint8Array(done).buffer;
+        console.log("Uncompressed size", newData.byteLength);
+        setLogData(newData);
+        setFileName(fileName.replace(/\.xz$/i, ""));
+        setProcessing(false);
+      });
+    }
+  }, [fileName, logData]);
 
   return (
     <>
