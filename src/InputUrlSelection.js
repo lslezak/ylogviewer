550e3d8a63c9e68798a3417c876d719702d04c4f
Ladislav Slezák <lslezak@suse.cz>
2023-01-31 09:50:30 +0100

    URL validation

--- src/InputUrlSelection.js
+++ src/InputUrlSelection.js
@@ -1,13 +1,44 @@
 
 import React, { useState } from "react";
-import {TextInput, ValidatedOptions} from '@patternfly/react-core';
+import { TextInput, HelperText, HelperTextItem, ValidatedOptions } from '@patternfly/react-core';
+
+const isValid = (url) => {
+  try {
+    return Boolean(new URL(url));
+  }
+  catch(e) {
+    return false;
+  }
+}
 
 export default function InputUrlSelection() {
   const [value, setValue] = useState('');
+  const [valid, setIsValid] = useState(true);
 
   const onChange = (value) => {
+    setIsValid(isValid(value));
     setValue(value);
   };
 
-  return <TextInput value={value} onChange={onChange} isRequired type="url" placeholder="HTTP URL"/>;
+  const displayError = (!valid && value !== "");
+  const validated = value === "" ? null : (valid ? ValidatedOptions.success :  ValidatedOptions.error)
+
+  return (
+    <>
+      <TextInput
+        value={value}
+        onChange={onChange}
+        type="url"
+        placeholder="HTTP URL"
+        validated={validated}
+      />
+      { displayError &&
+        <HelperText>
+          <HelperTextItem variant="error" hasIcon>
+            Invalid URL
+          </HelperTextItem>
+        </HelperText>
+      }
+    </>
+  );
 };
